35fea788c7b67763aeb2d08bf56d9d40918f2e94
ElricP <Elric@Elrics-MacBook-Pro.local>
2017-06-03 19:11:40 -0400

    Unity and UML

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex){
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
