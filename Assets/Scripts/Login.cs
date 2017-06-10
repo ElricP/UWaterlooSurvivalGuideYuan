6931bd028405e48716b7466bb4ac9e993768b339
j437lliu <liujiayi402@gmail.com>
2017-06-10 17:02:19 -0400

    Naive scripts
    :D

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class Login : MonoBehaviour {
+	public GameObject username;
+	public GameObject password;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Register.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class Register : MonoBehaviour {
+	public GameObject username;
+	public GameObject email;	
+	public GameObject password;
+	public GameObject confirmPassword;
+	private string Username;
+	private string Email;
+	private string Password;
+	private string ConfirmPassword;
+	private bool emailValid = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			if (username.GetComponent<InputField> ().isFocused) {
+				email.GetComponent<InputField> ().Select ();
+			}
+			if (email.GetComponent<InputField> ().isFocused) {
+				password.GetComponent<InputField> ().Select ();
+			}
+			if (password.GetComponent<InputField> ().isFocused) {
+				confirmPassword.GetComponent<InputField> ().Select ();
+			}
+		}
+		Username = username.GetComponent<InputField> ().text;
+		Email = email.GetComponent<InputField> ().text;
+		Password = password.GetComponent<InputField> ().text;
+		ConfirmPassword = confirmPassword.GetComponent<InputField> ().text;
+	}
+}
