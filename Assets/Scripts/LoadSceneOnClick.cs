7405eb3b73995106bcd6ada427b70621adc594d8
Ningshan Ouyang <nouyang@edu.uwaterloo.ca>
2017-06-11 00:32:17 -0400

    Add Setting Page + UI change

--- Assets/Scripts/LoadSceneOnClick.cs
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 
 public class LoadSceneOnClick : MonoBehaviour {
 
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+
+	public void LoadByName(string sceneName){
+		SceneManager.LoadScene (sceneName);
+	}
+
 	public void LoadByIndex(int sceneIndex){
 		SceneManager.LoadScene (sceneIndex);
 	}
