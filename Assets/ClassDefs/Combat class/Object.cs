24046f4ccb829310f38b6ec2b1a0b8b5e0ab72df
ElricP <yuan.elric@hotmail.com>
2017-06-11 12:25:51 -0400

    combat class folder added

---
+++ Assets/ClassDefs/Combat class/Object.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatObject : MonoBehaviour {
+	public int id;
+	public string icon;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
