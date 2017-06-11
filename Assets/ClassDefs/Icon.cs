fe7b1da468994192831894db24daa93f53d39a61
fanzhemeng <f25meng@uwaterloo.ca>
2017-06-11 11:26:57 -0400

    add Account, Character, Icon class defs

---
+++ Assets/ClassDefs/Icon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Icon : MonoBehaviour {
+
+	public int id;
+	public string name;
+	public Path pictuer;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
