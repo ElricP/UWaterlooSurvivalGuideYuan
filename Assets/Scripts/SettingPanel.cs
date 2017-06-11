b8d795f47a99e9752a687758cab84d7f5720921c
Ningshan Ouyang <nouyang@edu.uwaterloo.ca>
2017-06-11 12:27:22 -0400

    Setting

--- Assets/Scripts/SettingPanel.cs
+++ Assets/Scripts/SettingPanel.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class SettingPanel : MonoBehaviour {
 
+	private int BGMVolume;
+	private int SFXVolume;
+
 	// Use this for initialization
 	void Start () {
-		
+		BGMVolume = 50;
+		SFXVolume = 50;
 	}
 	
 	// Update is called once per frame
