6a1b31fb9c0b4cd15d1f2a6920e7378b37b3fc8e
ElricP <yuan.elric@hotmail.com>
2017-06-12 17:13:10 -0400

    Combat background, walls added. Changed build mode to IOS

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+	public int speed = 10000;
+	Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+		body = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 moveVec = new Vector2 (CrossPlatformInputManager.GetAxis("Horizontal"),CrossPlatformInputManager.GetAxis("Vertical")) * speed;
+		body.MovePosition (moveVec);
+	}
+}
