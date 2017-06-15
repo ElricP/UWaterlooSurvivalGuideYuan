bc7d48c863d092a8e814138f655e78c80f0776a3
ElricP <yuan.elric@hotmail.com>
2017-06-14 21:45:31 -0400

    Enabled player rotation, Imcomplete gun and bullet

---
+++ Assets/Scripts/VectorConvert.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VectorConvert {
+	public static Vector2 toVector2(this Vector3 vec3) {
+		return new Vector2 (vec3.x, vec3.y);
+
+	}
+}
