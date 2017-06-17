f1abc8e2e5f1a530448e97176265f2d3a4010ecf
ElricP <yuan.elric@hotmail.com>
2017-06-17 11:34:51 -0400

    Trivial change need to pull

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	[SerializeField]
+	private float xMax;
+	[SerializeField]
+	private float yMax;
+	[SerializeField]
+	private float xMin;
+	[SerializeField]
+	private float yMin;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
