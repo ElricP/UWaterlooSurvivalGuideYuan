c1772cae63e1885e092c154ed79055fb4a7ebf3d
Ningshan Ouyang <nouyang@edu.uwaterloo.ca>
2017-06-28 15:30:05 -0400

    Create shop panel

---
+++ Assets/Scripts/ShopPanel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopPanel : MonoBehaviour {
+
+	public Text goldText;
+	public Text diamondText;
+
+	void OnEnable () {
+		SetAllTexts ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetAllTexts() {
+		setGoldText (Account.account.GetGold());
+		setDiamondText (Account.account.GetDiamond());
+	}
+
+	private void setGoldText(int num) {
+		goldText.text = num.ToString();
+	}
+
+	private void setDiamondText(int num) {
+		diamondText.text = num.ToString();
+	}
+
+	public void Buy() {
+	}
+}
