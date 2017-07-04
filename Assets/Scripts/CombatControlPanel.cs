a99aa8545884e2050e9cbff90f475bac1a5c17ed
fanzhemeng <f25meng@uwaterloo.ca>
2017-07-04 18:42:22 -0400

    add reward panel
    (IMPORTANT) note that disabling panels in combat canvas requires first disabling "mobile control rig(script)" in Canvas.
    
    rewards are the same in each combat for now, it still needs an actual reward algorithm

---
+++ Assets/Scripts/CombatControlPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatControlPanel : MonoBehaviour {
+
+	public RewardPanel rewardPanel;
+	public GameObject controlPanel;
+	public PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		rewardPanel.gameObject.SetActive (false);
+		controlPanel.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.currentHealth <= 0) {
+			player.speed = 0;
+			player.attackSpeed = 0;
+			controlPanel.SetActive (false);
+			rewardPanel.gameObject.SetActive (true);
+			rewardPanel.Reward ();
+		}
+	}
+}
---
+++ Assets/Scripts/RewardPanel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewardPanel : MonoBehaviour {
+
+	public int expReward;
+	public int goldReward;
+	public int diamondReward;
+
+	public Text expText;
+	public Text goldText;
+	public Text diamondText;
+	public GameObject rewardPanel;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SetAllTexts() {
+		setExpText (expReward);
+		setGoldText (goldReward);
+		setDiamondText (diamondReward);
+	}
+
+	private void setExpText(int num) {
+		expText.text = "+" + num.ToString();
+	}
+
+	private void setGoldText(int num) {
+		goldText.text = "+" + num.ToString();
+	}
+
+	private void setDiamondText(int num) {
+		diamondText.text = "+" + num.ToString();
+	}
+
+	public void Reward() {
+		SetAllTexts ();
+		Account.account.SetExp (Account.account.GetExp () + expReward);
+		Account.account.SetGold (Account.account.GetGold () + goldReward);
+		Account.account.SetDiamond (Account.account.GetDiamond () + diamondReward);
+	}
+
+}
