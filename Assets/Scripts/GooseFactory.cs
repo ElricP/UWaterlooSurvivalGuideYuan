da59a502a20e92e1e208bb5baf697090790bc039
fanzhemeng <f25meng@uwaterloo.ca>
2017-07-20 13:03:56 -0400

    add goose generation

---
+++ Assets/Scripts/GooseFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooseFactory : MonoBehaviour {
+	[SerializeField]
+	private GameObject goose;
+	public float spawnTime = 5f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("GooseSpawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void GooseSpawn() {
+		Vector3 spawnPos = randomSpawnPosition ();
+		GameObject obj = Instantiate ( goose, spawnPos, Quaternion.identity); 
+		obj.SetActive (true);
+	}
+
+	Vector3 randomSpawnPosition() {
+		int xCoord = Random.Range (-4000, 4000);
+		int yCoord = Random.Range (-4000, 4000);
+		int zCoord = -90;
+		Vector3 pos = new Vector3 (xCoord, yCoord, zCoord);
+		return pos;
+	}
+}
