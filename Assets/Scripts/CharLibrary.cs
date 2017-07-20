1869b0b22f324bc102b879c7c90ab2e07a4e26bf
Ningshan Ouyang <nouyang@edu.uwaterloo.ca>
2017-07-20 16:55:08 -0400

    Change newton char

--- Assets/Scripts/CharLibrary.cs
+++ Assets/Scripts/CharLibrary.cs
@@ -6,39 +6,39 @@ public class CharLibrary: MonoBehaviour {
 
 	string[] nameDic = {
 		"Thomas RealTime",
-		"Newton Physics",
-		"Hortons Developer"
+		"Hortons Developer",
+		"Newton Physics"
 	};
 
 	string[] storyDic = {
 		"A student of CS452 real time OS. While playing trains all day, he became " +
 			"a multi-thread master and learned how to thread fork himself in order to survive on campus.",
-		"",
-		""
+		"A developer from Team Hortons. Almost invincible",
+		"A physics student in UW."
 	};
 
 	string[] abilityDic = {
 		"Thread fork - clone himself to help himself but increase the deadlock possibility ",
-		"",
-		"Hortons Developer"
+		"Developer Power - using Team Hortons power: become invincible for 5 seconds",
+		"Shadow step - ",
 	};
 
 	int[] attackDic = {
 		45,
-		10,
+		40,
 		35
 	};
 
 	int[] healthDic = {
 		100,
 		90,
-		35
+		50
 	};
 
 	int[] speedDic = {
 		55,
 		20,
-		35
+		40
 	};
 
 	public string GetName(int id) {
