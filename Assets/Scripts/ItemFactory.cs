10483a2dfa8afb8bc2b619d8af55b5be707aded3
Ningshan Ouyang <nouyang@edu.uwaterloo.ca>
2017-07-21 00:27:37 -0400

    Spawn donuts around Tim Hortons

--- Assets/Scripts/ItemFactory.cs
+++ Assets/Scripts/ItemFactory.cs
@@ -6,11 +6,13 @@ public class ItemFactory : MonoBehaviour {
 
 	//public PlayerController player;
 	public GameObject[] Items;
-	public float spawnTime = 20f;
+	public float spawnTime = 25f;
 	//public Transform spawnPoint;
+	Transform[] healthKitPosition;
 
 	// Use this for initialization
 	void Start () {
+		healthKitPosition = this.GetComponentsInChildren<Transform> ();
 		InvokeRepeating ("ItemSpawn", spawnTime, spawnTime);
 	}
 	
@@ -24,10 +26,19 @@ public class ItemFactory : MonoBehaviour {
 		/*if (player.currentHealth <= 0f) {
 			return;
 		}*/
+		GameObject obj;
 		int itemIndex = Random.Range (0, Items.Length-1);
-		Vector3 spawnPos = randomSpawnPosition ();
-		GameObject obj = Instantiate (Items [itemIndex], spawnPos, Quaternion.identity); 
-		obj.SetActive (true);
+		Vector3 spawnPos = itemIndex==1?randomHealthKitPosition():randomSpawnPosition ();
+		if (itemIndex == 1) {
+			GameObject[] kits = GameObject.FindGameObjectsWithTag ("HealthKit");
+			if (kits.Length < 14) {
+				obj = Instantiate (Items [itemIndex], spawnPos, Quaternion.identity); 
+				obj.SetActive (true);
+			}
+		} else {
+			obj = Instantiate (Items [itemIndex], spawnPos, Quaternion.identity); 
+			obj.SetActive (true);
+		}
 	}
 
 	Vector3 randomSpawnPosition() {
@@ -35,6 +46,14 @@ public class ItemFactory : MonoBehaviour {
 		int yCoord = Random.Range (-4000, 4000);
 		int zCoord = -90;
 		Vector3 pos = new Vector3 (xCoord, yCoord, zCoord);
+		return pos;
+	}
+
+	Vector3 randomHealthKitPosition() {
+		int positionIndex = Random.Range (0, healthKitPosition.Length-1);
+		Vector2 posK = healthKitPosition [positionIndex].position;
+		int zCoord = 0;
+		Vector3 pos = new Vector3 (posK.x, posK.y, zCoord);
 		return pos;
 	}
 }
